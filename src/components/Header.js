8715f36eff9f48575bba790059868f7c44f7cf20
Aashish Dhalla <adhalla@igategegdc.com>
2020-10-28 16:53:25 +0530

    React router setup

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import '../style/header.css'
+
+function Header() {
+    return (
+        <div>
+           <nav>
+              <div className="navbar">
+                  <h2>
+                      <Link to="/"> Streamer</Link>
+                  </h2>
+                  <ul className="items">
+                      <li> <Link to="/"> Streams</Link></li>
+                      {/* <li>Login</li> */}
+                  </ul>
+                  </div> 
+              
+           </nav>
+        </div>
+    )
+}
+
+export default Header
